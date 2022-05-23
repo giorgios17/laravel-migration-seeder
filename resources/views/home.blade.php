3c4131b27fc26c98c5695a272e6553aa26ad73db
giorgios17 <giosur@live.it>
2022-05-23 19:15:16 +0200

    foreach

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,12 +4,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
 
 <body>
-    <div>{{ $alltrain }}</div>
+    <div>Array: {{ $alltrain }}</div>
+    <div class='card'>
+        @foreach ($alltrain as $train)
+            <p>Azienda: {{ $train['azienda'] }}</p>
+            <p>Stazione di partenza: {{ $train['stazione_di_partenza'] }}</p>
+            <p>Stazione di arrivo: {{ $train['stazione_di_arrivo'] }}</p>
+            <p>Data partenza: {{ $train['data_partenza'] }}</p>
+            <p>Orario di partenza: {{ $train['orario_di_partenza'] }}</p>
+            <p>Orario di arrivo: {{ $train['Orario_di_arrivo'] }}</p>
+            <p>Codice treno: {{ $train['codice_treno'] }}</p>
+            <p>Numero carrozze: {{ $train['numero_carrozze'] }}</p>
+        @endforeach
+
+    </div>
 </body>
 
 </html>
